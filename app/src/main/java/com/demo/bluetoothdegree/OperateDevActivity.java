4784bdca39c8fa72eb52e94f60dcc18fe99c79dd
gaoyuanxin <gaoyuanxin@secoo.com>
2016-08-11 00:17:39 +0800

    jiaoling

--- app/src/main/java/com/demo/bluetoothdegree/OperateDevActivity.java
+++ app/src/main/java/com/demo/bluetoothdegree/OperateDevActivity.java
@@ -176,13 +176,17 @@ public class OperateDevActivity extends Activity {
         m_tvLocationZero.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                sRecordXiaH = sXiaH;
-                sRecordMianZ = sMianZ;
-                sRecordJianQ = sJianQ;
+                m_bPressZero = true;
+                if (m_bFirstZero == true) {
+                    sRecordXiaH = sXiaH;
+                    sRecordMianZ = sMianZ;
+                    sRecordJianQ = sJianQ;
 
-                m_tvJianQing1.setText(sRecordXiaH + "°");
-                m_tvXiaHe1.setText(sRecordJianQ + "°");
-                m_tvMianZhuan1.setText(sRecordMianZ + "°");
+                    m_tvJianQing1.setText(sRecordXiaH + "°");
+                    m_tvXiaHe1.setText(sRecordJianQ + "°");
+                    m_tvMianZhuan1.setText(sRecordMianZ + "°");
+                    m_bFirstZero = false;
+                }
             }
         });
 
@@ -200,6 +204,8 @@ public class OperateDevActivity extends Activity {
         });
     }
 
+    boolean m_bPressZero = false;
+    boolean m_bFirstZero = true;
     InputStream myInStream;
 
     private void startReceiveThread() throws IOException {
@@ -285,54 +291,83 @@ public class OperateDevActivity extends Activity {
         int nYaw = Integer.parseInt(sHex.substring(nIndex + 12, nIndex + 14) + sHex.substring(nIndex + 10, nIndex + 12), 16);
 
         double dbJianQingJiao = ((double) nRoll / (double) 32768) * (double) 180;//jianqingjiao
-        if (dbJianQingJiao > 180)
-            dbJianQingJiao -= 360;
         double dbXiaHeJiao = ((double) nPitch / (double) 32768) * (double) 180;//xiahejiao
-        if (dbXiaHeJiao > 180)
-            dbXiaHeJiao -= 360;
         double dbMianZhuanJiao = ((double) nYaw / (double) 32768) * (double) 180;//mianzhuan
-        if (dbMianZhuanJiao > 180)
-            dbMianZhuanJiao -= 360;
-
-        sJianQ = String.format("%.1f", dbJianQingJiao);
-        sXiaH = String.format("%.1f", dbXiaHeJiao);
-        sMianZ = String.format("%.1f", dbMianZhuanJiao);
 
         //如果从来没有按下过位置校零
         if (sRecordJianQ.isEmpty() && sRecordMianZ.isEmpty() && sRecordXiaH.isEmpty()) {
+            if (dbMianZhuanJiao > 180)
+                dbMianZhuanJiao -= 360;
+            if (dbXiaHeJiao > 180)
+                dbXiaHeJiao -= 360;
+            if (dbJianQingJiao > 180)
+                dbJianQingJiao -= 360;
+
+            sJianQ = String.format("%.1f", dbJianQingJiao);
+            sXiaH = String.format("%.1f", dbXiaHeJiao);
+            sMianZ = String.format("%.1f", dbMianZhuanJiao);
+
             m_tvJianQing1.setText(sXiaH + "°");
             m_tvXiaHe1.setText(sJianQ + "°");
             m_tvMianZhuan1.setText(sMianZ + "°");
         } else {
-            //按下过后，减法，然后显示在2上
-            String sminus1;
-            double minus1 = (Double.parseDouble(sXiaH) - Double.parseDouble(sRecordXiaH));
-            if (minus1 > 0)
-                sminus1 = "下";
-            else
-                sminus1 = "上";
+//如果突然又按下了校零
+            if (m_bPressZero) {
+                if (dbMianZhuanJiao > 180)
+                    dbMianZhuanJiao -= 360;
+                if (dbXiaHeJiao > 180)
+                    dbXiaHeJiao -= 360;
+                if (dbJianQingJiao > 180)
+                    dbJianQingJiao -= 360;
 
-            String sminus2;
-            double minus2 = (Double.parseDouble(sJianQ) - Double.parseDouble(sRecordJianQ));
-            if (minus2 > 0)
-                sminus2 = "左";
-            else
-                sminus2 = "右";
+                sJianQ = String.format("%.1f", dbJianQingJiao);
+                sXiaH = String.format("%.1f", dbXiaHeJiao);
+                sMianZ = String.format("%.1f", dbMianZhuanJiao);
 
-            String sminus3;
-            double minus3 = (Double.parseDouble(sMianZ) - Double.parseDouble(sRecordMianZ));
-            if (minus3 > 0)
-                sminus3 = "左";
-            else
-                sminus3 = "右";
-            //
-            s1 = String.format("%.1f", Math.abs(minus1));
-            s2 = String.format("%.1f", Math.abs(minus2));
-            s3 = String.format("%.1f", Math.abs(minus3));
-            //
-            m_tvJianQing2.setText(s1 + "°" + sminus1);
-            m_tvXiaHe2.setText(s2 + "°" + sminus2);
-            m_tvMianZhuan2.setText(s3 + "°" + sminus3);
+                sRecordXiaH = sXiaH;
+                sRecordMianZ = sMianZ;
+                sRecordJianQ = sJianQ;
+                m_tvJianQing1.setText(sRecordXiaH + "°");
+                m_tvXiaHe1.setText(sRecordJianQ + "°");
+                m_tvMianZhuan1.setText(sRecordMianZ + "°");
+                m_bPressZero = false;
+            } else {
+                sJianQ = String.format("%.1f", dbJianQingJiao);
+                sXiaH = String.format("%.1f", dbXiaHeJiao);
+                sMianZ = String.format("%.1f", dbMianZhuanJiao);
+                //按下过后，减法，然后显示在2上
+                String sminus1;
+                double minus1 = (Double.parseDouble(sXiaH) - Double.parseDouble(sRecordXiaH));
+                minus1 = minus1 > 180 ? minus1 - (double) 360 : minus1;
+                if (minus1 > 0)
+                    sminus1 = "左";
+                else
+                    sminus1 = "右";
+
+                String sminus2;
+                double minus2 = (Double.parseDouble(sJianQ) - Double.parseDouble(sRecordJianQ));
+                minus2 = minus2 > 180 ? minus2 - (double) 360 : minus2;
+                if (minus2 > 0)
+                    sminus2 = "下";
+                else
+                    sminus2 = "上";
+
+                String sminus3;
+                double minus3 = (Double.parseDouble(sMianZ) - Double.parseDouble(sRecordMianZ));
+                minus3 = minus3 > 180 ? minus3 - (double) 360 : minus3;
+                if (minus3 > 0)
+                    sminus3 = "左";
+                else
+                    sminus3 = "右";
+                //
+                s1 = String.format("%.1f", Math.abs(minus1));
+                s2 = String.format("%.1f", Math.abs(minus2));
+                s3 = String.format("%.1f", Math.abs(minus3));
+                //
+                m_tvJianQing2.setText(s1 + "°" + sminus1);
+                m_tvXiaHe2.setText(s2 + "°" + sminus2);
+                m_tvMianZhuan2.setText(s3 + "°" + sminus3);
+            }
         }
     }
 
